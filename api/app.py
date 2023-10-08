6e9e70340cceea656cd222c5c165696c2cde623a
Jose Ferreira <josebazjunior09@gmail.com>
2023-10-07 22:33:01 -0300

    [feat]: add connection with db

--- api/app.py
+++ api/app.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 
 from api.controller import main_router
+from api.models import user
+from api.config.db import init_db, engine
 
 app = FastAPI(
     title="API - Aluguel de imóveis",
     version="0.1.0",
     description="API Rest para sistema de aluguel de imóveis"
 )
+
+
+@app.on_event("startup")
+def start_db():
+    # refatorar apos criacao de repositorios
+    user.Base.metadata.create_all(bind=engine)
+    init_db()
+
 
 app.include_router(main_router)
