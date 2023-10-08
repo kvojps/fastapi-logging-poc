dd8d8e0a42f9d2d105cc336805988e141f56adde
Jose Ferreira <josebazjunior09@gmail.com>
2023-10-08 00:37:02 -0300

    [fix]: refactor user model

--- api/models/user.py
+++ api/models/user.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 
 from api.config.db import Base
 
@@ -9,5 +8,4 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    password = Column(String)
